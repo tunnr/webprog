0d1796538820fe001d5965e8841b883423e0f07d
machacsr <machacsroland@icloud.com>
2017-11-21 14:23:19 +0100

    login
    login form is ready for design

--- index.php
+++ index.php
@@ -10,6 +10,20 @@
             <section>
                 <!-- Here comes the login form -->
                 <!-- We want this in the center -->
+                <form action="">
+                    <h1>Login</h1>
+                    <div class="container">
+                        <label><b>Username or email</b></label>
+                        <input type="text" placeholder="Enter Username or Email" name="uname" required>
+
+                        <label><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" required>
+
+                        <button type="submit">Login</button>
+
+                        <a href="#">I forgot my password</a>
+                    </div>
+                </form>
             </section>
         </article>
         <!-- The login file doesn't have <aside> and <footer> tag -->
