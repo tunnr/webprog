8367d5557f2cecd322b9258cdd13f8f3d6c69bf5
machacsr <machacsroland@icloud.com>
2017-11-21 13:48:24 +0100

    admin/login plan
    admin website plan on admin/index.html and login website plan on index.html

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="../css/main.css">
+        <title>Multipurpose database website</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+
+            </nav>
+        </header>
+        <article>
+            <section>
+
+            </section>
+
+            <section>
+
+            </section>
+        </article>
+        <aside>
+
+        </aside>
+        <footer>
+
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="./css/login.css">
+        <title>Multipurpose database website</title>
+    </head>
+    <body>
+        <!-- The login file doesn't have <header> and <nav> tag -->
+        <article>
+            <section>
+                <!-- Here comes the login form -->
+                <!-- We want this in the center -->
+            </section>
+        </article>
+        <!-- The login file doesn't have <aside> and <footer> tag -->
+    </body>
+</html>
