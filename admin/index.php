c128160a32e50aeb14e9701810a550f11bfe5941
machacsr <machacsroland@icloud.com>
2017-11-22 15:28:27 +0100

    admin dashboard
    admin dashboard is ready for css

--- admin/index.php
+++ admin/index.php
@@ -7,20 +7,26 @@
     <body>
         <header>
             <nav>
-
+                //this icon is aligned to the left
+                <i>
+                //this menu is aligned to the right
+                <ul>
+                  <li><a href="#">Profile</a></li>
+                  <li><a href="#">Logout</a></li>
+                </ul>
             </nav>
         </header>
         <article>
             <section>
-
-            </section>
-
-            <section>
-
+                <iframe src="here is a JS variable, please initial homepage/homepage.php" scrolling="no"></iframe>
             </section>
         </article>
         <aside>
-
+            <ul>
+              <li><a href="#">User management</a></li>
+              <li><a href="#">Content management</a></li>
+              <li><a href="#">System management</a></li>
+            </ul>
         </aside>
         <footer>
 
